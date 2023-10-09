12e50e651d515591c31f5e6b41aff52a233dc6f1
logprose <logprose@umich.edu>
2023-10-08 21:47:01 -0400

    first push

---
+++ test.py
@@ -0,0 +1,15 @@
+#check
+
+test_int = 2
+
+test_int_2 = test_int + 4
+
+if (test_int_2 > test_int):
+    print("here")
+else:
+    print("not here")
+
+hello_world = "Hello world!"
+
+if (hello_world == "Hello world!"):
+    print("test 1")
