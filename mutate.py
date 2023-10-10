ec58dc76ee7511e8e110d99769318d4a8494cd98
logprose <logprose@umich.edu>
2023-10-10 15:53:03 -0400

    Working on random functionality

--- mutate.py
+++ mutate.py
@@ -35,23 +35,31 @@ class MyVisitor(ast.NodeTransformer):
     # The Visitor Pattern hides that information from us. Instead, we use
     # these visit_Num() functions and the like, which are called
     # automatically for us by the library. 
-    def visit_Num(self, node):
+    def visit_Num(self, node, generatedNum, num_mutants):
         print("Visitor sees a number: ", ast.dump(node), " aka ", astor.to_source(node))
         # Note how we never say "node.contents = 481" or anything like
         # that. We do not directly assign to nodes. Intead, the Visitor
         # Pattern hides that information from us. We use the return value
         # of this function and the new node we return is put in place by
         # the library. 
-        # Note: some students may want: return ast.Num(n=481) 
+        # Note: some students may want: return ast.Num(n=481)
+        if (generatedNum % 2 == 1):
+            return node
         return ast.Num(value=481, kind=None)
 
-    def visit_Str(self, node):
+    def visit_Str(self, node, generatedNum, num_mutants):
         print("Visitor sees a string: ", ast.dump(node), " aka ", astor.to_source(node))
         # Note: some students may want: return ast.Str(s=481)
+        if (generatedNum % 2 == 2):
+            return node
         return ast.Str(value="SE", kind=None)
-    def visit_Compare(self, node):
+    def visit_Compare(self, node, generatedNum, num_mutants):
+        if (generatedNum % 4 == 3):
+            return node
         return ast.Compare(left=node.left, ops=[ast.Lt()],comparators=[node.comparators[0]], kind=None)
-    def visit_BinOp(self, node):
+    def visit_BinOp(self, node, generatedNum, num_mutants):
+        if (generatedNum % 5 == 0):
+            return node
         if (type(node.op) == ast.Add):
             new_op = ast.Sub()
         elif (type(node.op) == ast.Sub):
@@ -61,7 +69,9 @@ class MyVisitor(ast.NodeTransformer):
         else:
             new_op = ast.Mult()
         return ast.BinOp(left=node.left, op=new_op, right=node.right, kind=None)
-    def visit_Assign(self, node):
+    def visit_Assign(self, node, generatedNum):
+        if (generatedNum % 6 == 0):
+            return node
         return ""
 # Instead of reading from a file, the starter code always processes in 
 # a small Python expression literally written in this string below:
@@ -79,12 +89,17 @@ print("Code is: ", code)
 print("Code's output is:") 
 exec(code)      # not needed for HW3
 print()
+num_mutatnts = int(args[2])
+
+
+for i in range(num_mutatnts):
+    print(i)
 
 
 # Now we will apply our transformation. 
 print("Applying AST transformation")
 tree = ast.parse(code)
-tree = MyVisitor().visit(tree)
+tree = MyVisitor().visit(tree, random.random(0, 100))
 # Add lineno & col_offset to the nodes we created
 ast.fix_missing_locations(tree)
 print("Transformed code is: ", astor.to_source(tree))
